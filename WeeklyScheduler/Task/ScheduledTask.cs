d608fcb7e89bd794c9de582782344b21b89a3d58
RWJames64 <rwjames64@juno.com>
2017-01-08 12:57:15 -0700

    Created ScheduledTask inheriting from Task

---
+++ WeeklyScheduler/Task/ScheduledTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyScheduler.Task
+{
+    class ScheduledTask : Task
+    {
+        public ScheduledTask(string title, string description, string time) : base(title, description)
+        {
+            Time = time;
+        }
+
+        public string Time { get; private set; }
+    }
+}
